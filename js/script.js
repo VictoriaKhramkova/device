b46579f4af77c0e3e7eccb2859b1534f261217b0
VictoriaKhramkova <122990727+VictoriaKhramkova@users.noreply.github.com>
2023-08-08 10:10:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-let catalogItem = document.querySelector('.navigation-catalog-item');
+// Меню
+let catalogItem = document.querySelector('.catalog-popover-wrapper');
 let buttonSubcatalog = document.querySelector('.button-subcatalog');
 
 catalogItem.classList.remove('catalog-closed-nojs');
+buttonSubcatalog.classList.remove('button-subcatalog-opened-nojs');
 
 buttonSubcatalog.addEventListener('click', function () {
   if (catalogItem.classList.contains('catalog-closed')) {
     catalogItem.classList.remove('catalog-closed');
     catalogItem.classList.add('catalog-opened');
+    buttonSubcatalog.classList.remove('button-subcatalog-opened');
+    buttonSubcatalog.classList.add('button-subcatalog-closed');
   } else {
     catalogItem.classList.add('catalog-closed');
     catalogItem.classList.remove('catalog-opened');
+    buttonSubcatalog.classList.remove('button-subcatalog-closed');
+    buttonSubcatalog.classList.add('button-subcatalog-opened');
   }
 });
